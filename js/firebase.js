d199bc5081ee350a003114dda6ec36f54b0bf1e3
deccalcorrect <kemalicloud123@gmail.com>
2026-08-08 05:38:07 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-import {
-    getFirestore
+import { 
+    getFirestore 
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 import {
@@ -10,19 +10,12 @@ import {
 
 
 const firebaseConfig = {
-
     apiKey: "AIzaSyBqwg0Z4FamOfbRqQYG4cpfnD3souVU8E",
-
     authDomain: "bluelock-3f153.firebaseapp.com",
-
     projectId: "bluelock-3f153",
-
     storageBucket: "bluelock-3f153.firebasestorage.app",
-
     messagingSenderId: "406394448785",
-
     appId: "1:406394448785:web:1f688381952cd50c948830"
-
 };
 
 

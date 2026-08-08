2734ff1c3c137404ce8321664ac4ac1b3134c102
deccalcorrect <kemalicloud123@gmail.com>
2026-08-08 03:35:22 +0300

    Add files via upload

---
+++ js/cloudinary.js
@@ -0,0 +1,26 @@
+const cloudName = "y3in3pxq";
+const uploadPreset = "bluelock_players";
+
+
+async function uploadPlayerImage(file) {
+
+    const url =
+    `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`;
+
+
+    const formData = new FormData();
+
+    formData.append("file", file);
+    formData.append("upload_preset", uploadPreset);
+
+
+    const response = await fetch(url,{
+        method:"POST",
+        body:formData
+    });
+
+
+    const data = await response.json();
+
+    return data.secure_url;
+}

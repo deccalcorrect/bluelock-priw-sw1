94cb69c5e6eee549b1443089c1f09398d2742d47
deccalcorrect <kemalicloud123@gmail.com>
2026-08-08 06:34:39 +0300

    Update cloudinary.js

--- js/cloudinary.js
+++ js/cloudinary.js
@@ -1,42 +1,44 @@
-const cloudName = "y3in3pxq";
-
-const uploadPreset = "bluelock_players";
-
-
 export async function uploadPlayerImage(file){
 
-const url =
-`https://api.cloudinary.com/v1_1/${cloudName}/image/upload`;
+
+    const url =
+    "https://api.cloudinary.com/v1_1/REPLACE_CLOUD_NAME/image/upload";
 
 
-const formData = new FormData();
+    const formData =
+    new FormData();
 
 
-formData.append(
-"file",
-file
-);
+    formData.append(
+        "file",
+        file
+    );
 
 
-formData.append(
-"upload_preset",
-uploadPreset
-);
+    formData.append(
+        "upload_preset",
+        "REPLACE_UPLOAD_PRESET"
+    );
 
 
 
-const response = await fetch(
-url,
-{
-method:"POST",
-body:formData
-}
-);
+    const response =
+    await fetch(
+        url,
+        {
+            method:"POST",
+            body:formData
+        }
+    );
 
 
-const data = await response.json();
+
+    const data =
+    await response.json();
 
 
-return data.secure_url;
+
+    return data.secure_url;
+
 
 }

526554756aa67e7c23249c3dcc006de1f25ecf38
deccalcorrect <kemalicloud123@gmail.com>
2026-08-08 05:49:34 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,29 +1,48 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { initializeApp } from 
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-import { 
-    getFirestore 
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 import {
-    getAuth
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+getFirestore
+}
+from
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+
+import {
+getAuth
+}
+from
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBqwg0Z4FamOfbRqQYG4cpfnD3souVU8E",
-    authDomain: "bluelock-3f153.firebaseapp.com",
-    projectId: "bluelock-3f153",
-    storageBucket: "bluelock-3f153.firebasestorage.app",
-    messagingSenderId: "406394448785",
-    appId: "1:406394448785:web:1f688381952cd50c948830"
+
+apiKey: "AIzaSyBqwg0Z4FamOfbRqQYG4cpfnD3souVU8E",
+
+authDomain: "bluelock-3f153.firebaseapp.com",
+
+projectId: "bluelock-3f153",
+
+storageBucket: "bluelock-3f153.firebasestorage.app",
+
+messagingSenderId: "406394448785",
+
+appId: "1:406394448785:web:1f688381952cd50c948830"
+
 };
+
 
 
 const app = initializeApp(firebaseConfig);
 
 
+
 export const db = getFirestore(app);
 
+
 export const auth = getAuth(app);
+
 
 export { app };
